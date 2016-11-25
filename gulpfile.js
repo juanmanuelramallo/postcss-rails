4b425b3c38d8134bed02d09e333155812d250e3c
JUAN MANUEL RAMALLO <RAMALLOJUANM@GMAIL.COM>
2016-11-25 17:41:04 -0300

    All is fine

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var browserSync = require('browser-sync').create()
+var del = require('del');
+var rename = require('gulp-rename')
+
+var rucksack = require('rucksack-css')
+var cssnext = require('postcss-cssnext')
+var autoprefixer = require('autoprefixer')
+var cssnested = require('postcss-nested')
+var mixins = require('postcss-mixins')
+var lost = require('lost')
+var atImport = require('postcss-import')
+var csswring = require('csswring')
+var materialShadow = require('postcss-material-shadow-helper')
+var instagram = require('postcss-instagram')
+var fontMagician = require('postcss-font-magician')
+var getColor = require('postcss-get-color')
+
+gulp.task('clean', function () {
+  del('./public/assets/stylesheets');
+});
+
+gulp.task('css', function(){
+  var processors = [
+    atImport({
+      path: ["./gulp/stylesheets"]
+    }),
+    fontMagician(),
+    getColor(),
+    cssnested,
+    lost(),
+    rucksack(),
+    cssnext({ browsers: ['> 5%', 'last 2 versions']}),
+    materialShadow(),
+    instagram()
+    // csswring()
+  ]
+  return gulp.src('./gulp/stylesheets/main.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./public/assets/stylesheets'))
+    .pipe(rename({
+            suffix: '.self'
+        }))
+    .pipe(browserSync.stream() )
+})
+
+gulp.task('serve', function () {
+  browserSync.init({
+    proxy: 'localhost:3000',
+    files: ['./app/views/**']
+  })
+})
+
+gulp.task('watch', function(){
+  gulp.watch('./gulp/stylesheets/*.css', ['css'])
+  gulp.watch('./app/views/**/*.html').on('change', function(){
+     browserSync.reload()
+   })
+})
+
+gulp.task('default', ['watch', 'serve'])
